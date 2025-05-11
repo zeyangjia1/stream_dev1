01896626969a2e58fa0187874d6b78dc8cb0f666
jzyayang <jzyayang123>
2025-05-11 21:28:16 +0800

    add 达摩盘项目构建 or 设计文档

---
+++ DamoPan/src/main/java/test.java
@@ -0,0 +1,13 @@
+import org.bouncycastle.pqc.legacy.math.linearalgebra.PolynomialRingGF2;
+
+/**
+ * @Package PACKAGE_NAME.test
+ * @Author zeyang_jia
+ * @Date 2025/5/11 21:09
+ * @description: 达摩盘 项目 测试运行
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+    }
+}
